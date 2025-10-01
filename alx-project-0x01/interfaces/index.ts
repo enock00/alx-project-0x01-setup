6a5e8dcd4334d0231d7ca585564ee15ab94fd18f
Enock <enockzy54@gmail.com>
2025-10-01 23:59:48 +0300

    Implement the User Modal

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -37,5 +37,5 @@ export interface UserData {
 export interface UserModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSave: (user: UserData) => void;
+  onSubmit: (post: UserProps) => void;
 }
