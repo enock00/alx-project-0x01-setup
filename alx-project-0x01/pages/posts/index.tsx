55549a87433e90a0830bfa2eefff89306d53fbae
Enock <enockzy54@gmail.com>
2025-10-01 22:16:02 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Posts</h1>
-      <PostCard title="First Post" description="This is my very first post!" />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-8 flex-grow">
+        <h1 className="text-3xl font-bold mb-6">Posts</h1>
+        <PostCard title="First Post" description="This is my very first post!" />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
 import Button from "@/components/common/Button";
 
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Users</h1>
-      <Button title="Add User" onClick={() => alert("Add user clicked!")} />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-8 flex-grow">
+        <h1 className="text-3xl font-bold mb-6">Users</h1>
+        <Button title="Add User" onClick={() => alert("Add user clicked!")} />
+      </main>
     </div>
   );
 };
